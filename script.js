27ad644e1036cbaa4b3f995cdd5587bcaa999bf5
Dimka Atanassov <dimka@udacity.com>
2015-02-15 17:37:22 -0800

    Added all the resume files

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.menu').accordion();
+
+
+    $('.navbar li').click(function(e) {
+    	$('.navbar li.active').removeClass('active');
+    	var $this = $(this);
+    		if (!$this.hasClass('active')) {
+        		$this.addClass('active');
+    		}
+    	//e.preventDefault();
+		});
+    });
+
